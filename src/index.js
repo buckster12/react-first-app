1904d849cbb8e9aee62765d60c1cf68088fc0f64
buckster12 <roboks@mail.ru>
2019-03-15 21:51:09 +0300

    char counter

--- src/index.js
+++ src/index.js
@@ -1,10 +1,64 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+class MyComponent extends Component {
+    // static propTypes = {
+    //     name:
+    // }
+
+
+    // const
+    // name = 'noname';
+
+    static defaultProps = {
+        text: ''
+    };
+
+    // static defaultState = {
+    //     name: 'n/a'
+    // };
+
+    constructor(props) {
+        super(props);
+
+        // this.setState({
+            // name: '11'
+        // });
+
+        // this.props.name = 'noname';
+        this.makeNoise = this.makeNoise.bind(this);
+        this.countChars = this.countChars.bind(this);
+
+        this.state = {
+            text: '',
+        }
+    }
+
+    propTypes: {
+        text: React.PropTypes.string.isRequired,
+    };
+
+    countChars(e) {
+        this.setState({
+            text: e.target.value,
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <textarea onChange={this.countChars} defaultValue={this.props.text} name="" id="" cols="30" rows="10"></textarea>
+                <h3>{this.state.text.length}</h3>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<MyComponent/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
